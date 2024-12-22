5c1e4d9a659fa1cf6d5d11ef63f4f0f9f76ef63e
Robert Saylor <rsaylor@pathful.com>
2024-12-22 06:55:10 -0500

    added variables to the modules

--- variables.tf
+++ variables.tf
@@ -1,10 +1,7 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
