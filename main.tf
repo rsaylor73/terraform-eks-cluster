9c2c9d4aa62dd0bbafbebe4bf24d0ad1ce1e499b
Robert Saylor <rsaylor@pathful.com>
2024-12-22 19:11:09 -0500

    added variables to the modules

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "eks" {
   version = "20.31.6"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.32"
+  cluster_version = "1.31"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
