8e20e397e6171019925037a5bf7da6cbc115c705
Robert Saylor <rsaylor@pathful.com>
2024-12-22 20:02:27 -0500

    added variables to the modules

--- variables.tf
+++ variables.tf
@@ -75,12 +75,12 @@ variable "node_group1_min_size" {
 
 variable "node_group1_max_size" {
   type = string
-  default = "2"
+  default = "1"
 }
 
 variable "node_group1_desired_size" {
   type = string
-  default = "2"
+  default = "1"
 }
 
 variable "node_group2_name" {
@@ -100,10 +100,10 @@ variable "node_group2_min_size" {
 
 variable "node_group2_max_size" {
   type = string
-  default = "2"
+  default = "1"
 }
 
 variable "node_group2_desired_size" {
   type = string
-  default = "2"
+  default = "1"
 }
